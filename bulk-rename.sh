8ed9eae80bd8dbcc810465263366a58b5caf03ee
jonboydell <jonboydell@hotmail.com>
2017-10-20 13:23:45 +0100

    Update bulk-rename.sh

--- bulk-rename.sh
+++ bulk-rename.sh
@@ -1,11 +1,7 @@
+#!/bin/bash
+
 find . -name '*.ogg' -exec sh -c 'mv $0 $(basename $(dirname $0))-$(basename $0)' {} \;
-
-
 find . -name '*.wwise' -exec sh -c 'ww2ogg $0 --pcb $HOME/Projects/ww2ogg022/packed_codebooks_aoTuV_603.bin' {} \;
-
 ww2ogg ${item} --pcb ${WW2OGG_HOME}/packed_codebooks_aoTuV_603.bin
-
-
 find . -name '*.ogg' -exec sh -c 'revorb $0' {} \;
-
 find . -name '*.ogg' -exec sh -c 'rm $0' {} \;
